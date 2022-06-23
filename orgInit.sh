23e3abbabb55bed23849e08133d3df6ba1887943
Demen Selcan <demen.selcan@gmail.com>
2022-06-23 10:33:14 +0200

    -updated hc feature to HealthCloudAddOn

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
+sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 15
 sfdx force:package:install --package "04t4W000002g1ObQAI" --wait 10 --publishwait 10
 sfdx force:source:push --forceoverwrite --loglevel fatal --wait 30
 sfdx force:user:create -a pattyPatientCoordination -f users/hc_patientcoordination.json
